9f2c9d512f8913f9b306bda1dd60a82b0bbbc91c
Sandeep <sandeephborhade@gmail.com>
2023-07-07 07:49:00 -0400

    sample payment form

--- mf-payment/src/app/payment/payment.component.ts
+++ mf-payment/src/app/payment/payment.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,8 +6,12 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.sass']
+  styleUrls: ['./payment.component.sass'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PaymentComponent {
-
+  onSubmit(): void {
+    // Handle form submission logic here, such as sending payment details to a server or performing client-side validation.
+    console.log('Payment submitted');
+  }
 }
